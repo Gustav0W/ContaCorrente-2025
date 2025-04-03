b1cc6e97a17e4f59dc1401551817f556204ce03f
Gustavo <waltervgustavo@gmail.com>
2025-04-03 02:12:57 -0300

    Créditos para o Kauan, ajudou muito. Quero ver uma aula depois.

---
+++ ContaCorrente.ConsoleApp/Conta.cs
@@ -0,0 +1,85 @@
+﻿namespace ContaCorrente.ConsoleApp
+{
+    public class ContaCorrente
+    {
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
+        public double Limite { get; set; }
+        public List<string> movimentacoes;
+
+        public ContaCorrente(int numero, double saldoInicial, double limite)
+        {
+            Numero = numero;
+            Saldo = saldoInicial;
+            Limite = limite;
+            movimentacoes = new List<string>();
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("Não tem como sacar valores negativos.");
+                return false;
+            }
+            if (Saldo + Limite >= valor)
+            {
+                Saldo -= valor;
+                movimentacoes.Add($"Saque: -R${valor:F2}");
+                Console.WriteLine($"Você sacou R${valor:F2}");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+                movimentacoes.Add($"Depósito: +R${valor:F2}");
+                Console.WriteLine($"Você depositou R${valor:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido para depósito.");
+            }
+        }
+
+        public bool TransferirPara(ContaCorrente destino, double valor)
+        {
+            if (Sacar(valor))
+            {
+                destino.Depositar(valor);
+                movimentacoes.Add($"O valor enviado foi de: -R${valor:F2} para a conta {destino.Numero}");
+                destino.movimentacoes.Add($"Você recebeu o valor de: +R${valor:F2} da conta {Numero}");
+                Console.WriteLine($"A transferência de R${valor:F2} para a conta {destino.Numero} foi realizada com sucesso!");
+                return true;
+            }
+            return false;
+        }
+
+        public void ExibirExtrato()
+        {
+            Console.WriteLine($"\nExtrato da conta {Numero}:");
+            foreach (var item in movimentacoes)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Saldo atual: R${Saldo:F2}\n");
+        }
+
+        public void MostrarSaldoFinal()
+        {
+            Console.WriteLine($"Saldo final da conta {Numero}: R${Saldo:F2}");
+        }
+    }
+}
+
+ 
+
+
--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -1,2 +1,38 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿namespace ContaCorrente.ConsoleApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente(12, 1000, 10);
+            conta1.movimentacoes = new List<string>();
+
+            conta1.Sacar(200);
+
+            conta1.Depositar(300);
+
+            conta1.Depositar(500);
+
+            conta1.Sacar(200);
+
+            ContaCorrente conta2 = new ContaCorrente(13, 300, 0);
+
+            conta1.movimentacoes = new List<string>();
+
+            conta1.TransferirPara(conta2, 400);
+
+            conta1.ExibirExtrato();
+
+            conta1.MostrarSaldoFinal();
+
+            Console.Write("\n");
+
+            conta2.ExibirExtrato();
+
+            conta2.MostrarSaldoFinal();
+
+
+
+        }
+    }
+}
